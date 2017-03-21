7b9cda4ef145deb0f69b073753ef123d33e20466
DragosB <dragosbad2009@gmail.com>
2017-03-21 12:52:36 +0200

    Update

--- src/main/repository/FlightRepo.java
+++ src/main/repository/FlightRepo.java
@@ -1,9 +1,5 @@
 package repository;
 
-/**
- * Created by Dragos on 3/15/2017.
- */
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -19,7 +15,7 @@ public class FlightRepo
 {
     private List<Flight> flights = new ArrayList<Flight>();
 
-    private Connection connection = null;
+    private Connection connection;
     private Statement statement;
 
 
@@ -31,18 +27,17 @@ public class FlightRepo
 
         try
         {
-            Class.forName("com.mysql.jdbc.Driver").newInstance();
-            Connection con = DriverManager.getConnection(url, user, password);
+            Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+            connection = DriverManager.getConnection(url, user, password);
 
-            Statement stt = con.createStatement();
-            stt.execute("CREATE DATABASE IF NOT EXISTS flights");
-            stt.execute("USE flights");
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE DATABASE IF NOT EXISTS flights");
+            statement.execute("USE flights");
         }
         catch (Exception e)
         {
             e.printStackTrace();
         }
-
         String query = "select * from flight";
         ResultSet result = statement.executeQuery(query);
         while(result.next())
@@ -55,8 +50,8 @@ public class FlightRepo
             Flight flight = new Flight(id,dest,airp,frees,dateh);
 
             flights.add(flight);
-            System.out.println("Done2");
         }
+        System.out.println("Done2");
     }
     @Bean
     public List<Flight> getAll()
